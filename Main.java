61d7f47f8381db0654f7f5fc637a4f0a6d5075e6
LAB-USR-LCENTRO <LAB-USR-LCENTRO@grupoutp.net>
2026-04-28 13:27:26 -0500

    Version 1 en main: implementacion inicial con error de signo

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-package ecuacionsegundogradomain;
+package ecuacionsegundogradoramas;
 
 import java.util.Scanner;
 
@@ -7,44 +7,22 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        double a, b, c;
-        double discriminante, x1, x2;
+        int a, b, c;
+        double x1, x2;
 
         System.out.print("Ingrese a: ");
-        a = sc.nextDouble();
+        a = sc.nextInt();
 
         System.out.print("Ingrese b: ");
-        b = sc.nextDouble();
+        b = sc.nextInt();
 
         System.out.print("Ingrese c: ");
-        c = sc.nextDouble();
+        c = sc.nextInt();
 
-        if (a == 0) {
-            System.out.println("No es una ecuacion de segundo grado porque a no puede ser 0.");
-            return;
-        }
+        x1 = (b + Math.sqrt((b * b) - (4 * a * c))) / (2 * a);
+        x2 = (b - Math.sqrt((b * b) - (4 * a * c))) / (2 * a);
 
-        discriminante = (b * b) - (4 * a * c);
-
-        if (discriminante > 0) {
-
-            x1 = (-b + Math.sqrt(discriminante)) / (2.0 * a);
-            x2 = (-b - Math.sqrt(discriminante)) / (2.0 * a);
-
-            System.out.println("Dos raices reales diferentes:");
-            System.out.println("Raiz 1: " + x1);
-            System.out.println("Raiz 2: " + x2);
-
-        } else if (discriminante == 0) {
-
-            x1 = (-b) / (2.0 * a);
-
-            System.out.println("Una raiz real doble:");
-            System.out.println("Raiz: " + x1);
-
-        } else {
-
-            System.out.println("No existen raices reales.");
-        }
+        System.out.println("Raiz 1: " + x1);
+        System.out.println("Raiz 2: " + x2);
     }
 }
